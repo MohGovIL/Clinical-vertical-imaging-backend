581ebcb4ade847c42dbd22141fb60bf5b09482fb
Amiel Elboim <amielel@matrix.co.il>
2019-12-04 08:39:40 +0200

    fix

--- acl/Roles_ids.php
+++ acl/Roles_ids.php
@@ -18,24 +18,11 @@ class Roles_ids{
 
             self::$instance->all_roles_ids = array(
                 'admin_write' => getAclIdNumber('Administrators', 'write'),
-                'doctor_write' => getAclIdNumber('Vaccine doctors', 'write'),
-                'doctor_view' => getAclIdNumber('Vaccine doctors', 'view'),
-                'nurse_write' => getAclIdNumber('Vaccine nurses', 'write'),
-                'nurse_view' => getAclIdNumber('Vaccine nurses', 'view'),
-                'receptionist_write' => getAclIdNumber('Vaccine receptionists', 'write'),
-                'receptionist_view' => getAclIdNumber('Vaccine receptionists', 'view'),
-                'epidemiology_write' => getAclIdNumber('Epidemiology', 'write'),
-                'epidemiology_view' => getAclIdNumber('Epidemiology', 'view'),
                 'application_manager_write' => getAclIdNumber('Application Manager', 'write'),
                 'application_manager_view' => getAclIdNumber('Application Manager', 'view'),
-
-                'rabies_receptionists_write' => getAclIdNumber('Rabies receptionists', 'write'),
-                'rabies_receptionists_view' => getAclIdNumber('Rabies receptionists', 'view'),
-                'rabies_nurse_write' => getAclIdNumber('Rabies nurse', 'write'),
-                'rabies_nurse_view' => getAclIdNumber('Rabies nurse', 'view'),
-                'rabies_doctor_write' => getAclIdNumber('Rabies doctors', 'write'),
-                'rabies_doctor_view' => getAclIdNumber('Rabies doctors', 'view'),
-
+                //EXAMPLE
+                'receptionist_write' => getAclIdNumber('Vaccine receptionists', 'write'),
+                'receptionist_view' => getAclIdNumber('Vaccine receptionists', 'view'),
 
             );
             return self::$instance;
@@ -58,6 +45,7 @@ class Roles_ids{
     }
 
 }
+
 
 
 
