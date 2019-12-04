045da0c01f61ff0d1fe33df6933afaff666e3507
Amiel Elboim <amielel@matrix.co.il>
2019-12-04 08:47:28 +0200

    fix sql

--- sql/install.sql
+++ sql/install.sql
@@ -1,1 +1,3 @@
-INSERT INTO `list_options` (`list_id`, `option_id`, `title`, `seq`, `is_default`, `option_value`, `mapping`, `notes`, `codes`, `toggle_setting_1`, `toggle_setting_2`, `activity`, `subtype`, `edit_options`, `timestamp`) VALUES ('apps', 'react-dev', '../client-app/dev-mode/build/index.html#imaging', '100', '1', '0', '', NULL, '', '0', '0', '1', '', '1', '2017-07-23 09:33:02');
+INSERT INTO `list_options` (`list_id`, `option_id`, `title`, `seq`, `is_default`, `option_value`, `mapping`, `notes`, `codes`, `toggle_setting_1`, `toggle_setting_2`, `activity`, `subtype`, `edit_options`, `timestamp`) VALUES ('apps', 'react-dev', '../client-app/dev-mode/build/index.html', '100', '1', '0', '', NULL, '', '0', '0', '1', '', '1', '2017-07-23 09:33:02');
+
+UPDATE globals SET gl_value = 'style_clinikal_generic.css' WHERE gl_name = 'css_header';
