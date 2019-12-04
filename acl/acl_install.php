45dfe81e4445f842a4abe0c21785bbdc6e38006d
Amiel Elboim <amielel@matrix.co.il>
2019-12-04 07:33:49 +0200

    empty acl

---
+++ acl/acl_install.php
@@ -0,0 +1,10 @@
+<?php
+//EXAMPLE
+/* Vaccines groups */
+//$receptionist_view = @addNewACL('Vaccine receptionists', 'receptionist', 'view', 'Things that receptionists can read but not modify');
+//$receptionist_write = @addNewACL('Vaccine receptionists', 'receptionist', 'write', 'Things that receptionists can modify');
+
+
+
+//Insert the 'notes' object from the 'patients' section
+//updateAcl($receptionist_write, 'Vaccine receptionists', 'patients', 'Patients', 'notes', 'Patient Notes (write,addonly optional)', 'write');
---
+++ acl/acl_upgrade.php
@@ -0,0 +1,11 @@
+<?php
+
+return $ACL_UPGRADE = array(
+
+    //example
+   /* '0.2.0' => function () {
+        addObjectAcl('admin', 'Administration', 'test', 'Test');
+        updateAcl(Roles_ids::instance()->admin_write, 'Administrators', 'admin', 'Administration', 'test', 'Test', 'write');
+    },*/
+
+);
