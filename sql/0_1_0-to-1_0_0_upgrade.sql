5167c08bfc5a862112c4fdd728cf385c0835d21e
eyalvo-matrix <eyalvo.matrix@gmail.com>
2020-02-19 18:54:27 +0200

    upgrase

--- sql/0_1_0-to-1_0_0_upgrade.sql
+++ sql/0_1_0-to-1_0_0_upgrade.sql
@@ -215,3 +215,9 @@ CREATE TABLE `event_codeReason_map` (
 
 ALTER TABLE event_codeReason_map ADD PRIMARY KEY (event_id, option_id);
 #EndIf
+
+ALTER TABLE event_codeReason_map DROP PRIMARY KEY;
+ALTER TABLE event_codeReason_map ADD PRIMARY KEY (event_id, option_id);
+
+
+
