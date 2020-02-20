811ef7eebd3c0440f5deacd7cfe479d6bc1a9cae
shaharzi <shaharzi@matrix.co.il>
2020-02-20 11:06:11 +0200

    PC-241

--- sql/0_1_0-to-1_0_0_upgrade.sql
+++ sql/0_1_0-to-1_0_0_upgrade.sql
@@ -157,13 +157,176 @@ reason_code  INT(6) UNSIGNED
 ALTER TABLE facility ADD active int DEFAULT 1;
 #EndIf
 
+#IfNotTable fhir_value_sets
+CREATE TABLE `fhir_value_sets` (
+    `id` VARCHAR(125) NOT NULL,
+    `title` VARCHAR(125) NOT NULL,
+    `active` BOOLEAN NOT NULL DEFAULT 1,
+    PRIMARY KEY(`id`)
+) ENGINE=INNODB DEFAULT CHARSET=UTF8;
+#EndIf
 
--- Example for Eyal
-#IfNotRow2D list_options list_id lists option_id clinikal_reason_code
+#IfNotTable fhir_value_set_systems
+CREATE TABLE `fhir_value_set_systems` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `vs_id` VARCHAR(125) NOT NULL,
+    `system` VARCHAR(125) NOT NULL,
+    `type` ENUM('All', 'Partial', 'Exclude', 'Filter') NOT NULL,
+    `filter` VARCHAR(125) DEFAULT NULL,
+    PRIMARY KEY(`id`)
+) ENGINE=INNODB DEFAULT CHARSET=UTF8;
+#EndIf
+
+#IfNotTable fhir_value_set_codes
+CREATE TABLE `fhir_value_set_codes` (
+    `vss_id` INT NOT NULL,
+    `code` VARCHAR(125) NOT NULL,
+    PRIMARY KEY(`vss_id`, `code`)
+) ENGINE=INNODB DEFAULT CHARSET=UTF8;
+#EndIf
+
+-- no appropriate condition
+UPDATE `list_options` SET `notes` = ''
+    WHERE `list_id` IN ('clinikal_service_categories', 'clinikal_service_types') OR `option_id` IN ('clinikal_service_categories', 'clinikal_service_types');
+
+#IfNotRow2D list_options list_id clinikal_service_types option_id 7
 INSERT INTO `list_options` (`list_id`, `option_id`, `title`, `seq`, `is_default`, `option_value`, `mapping`, `notes`, `codes`, `toggle_setting_1`, `toggle_setting_2`, `activity`, `subtype`, `edit_options`) VALUES
-('lists', 'clinikal_reason_code', 'Clinikal Reason code', 0, 0, 0, '', 'BFBFBF|0', '', 0, 0, 1, '', 1),
-('clinikal_reason_code', '1', 'shoulder', 10, 0, 0, '', 'FFFF2B|0', '', 0, 0, 1, '', 1),
-('clinikal_reason_code', '2', 'ankle', 20, 0, 0, '', 'FFFF2B|0', '', 0, 0, 1, '', 1);
+('clinikal_service_types', '7', 'Biopsy', 15, 0, 0, '', '', '', 0, 0, 1, '', 1);
+#EndIf
+
+#IfNotRow2D list_options list_id lists option_id clinikal_reason_codes
+INSERT INTO `list_options` (`list_id`, `option_id`, `title`, `seq`, `is_default`, `option_value`, `mapping`, `notes`, `codes`, `toggle_setting_1`, `toggle_setting_2`, `activity`, `subtype`, `edit_options`) VALUES
+('lists', 'clinikal_reason_codes', 'Clinikal Reason Codes', 0, 0, 0, '', '', '', 0, 0, 1, '', 1),
+('clinikal_reason_codes', '1', 'Shoulder', 10, 0, 0, '', '1', '', 0, 0, 1, '', 1),
+('clinikal_reason_codes', '2', 'Ankle', 20, 0, 0, '', '1', '', 0, 0, 1, '', 1),
+('clinikal_reason_codes', '3', 'Foot', 30, 0, 0, '', '1', '', 0, 0, 1, '', 1),
+('clinikal_reason_codes', '4', 'Hand', 40, 0, 0, '', '1', '', 0, 0, 1, '', 1),
+('clinikal_reason_codes', '5', 'Upper Abdomen', 50, 0, 0, '', '1', '', 0, 0, 1, '', 1),
+('clinikal_reason_codes', '6', 'Upper And Lower Abdomen', 60, 0, 0, '', '1', '', 0, 0, 1, '', 1),
+('clinikal_reason_codes', '7', 'Lower Abdomen And Kidney And Urinary Tract', 70, 0, 0, '', '1', '', 0, 0, 1, '', 1),
+('clinikal_reason_codes', '8', 'Head And Neck', 80, 0, 0, '', '1', '', 0, 0, 1, '', 1),
+('clinikal_reason_codes', '9', 'Breast', 90, 0, 0, '', '1', '', 0, 0, 1, '', 1),
+('clinikal_reason_codes', '10', '3D Breast', 100, 0, 0, '', '1', '', 0, 0, 1, '', 1),
+('clinikal_reason_codes', '11', 'Breast', 10, 0, 0, '', '7', '', 0, 0, 1, '', 1),
+('clinikal_reason_codes', '12', 'Mammography', 10, 0, 0, '', '2', '', 0, 0, 1, '', 1),
+('clinikal_reason_codes', '13', 'Shoulder', 10, 0, 0, '', '3', '', 0, 0, 1, '', 1),
+('clinikal_reason_codes', '14', 'Ankle', 20, 0, 0, '', '3', '', 0, 0, 1, '', 1),
+('clinikal_reason_codes', '15', 'Foot', 30, 0, 0, '', '3', '', 0, 0, 1, '', 1),
+('clinikal_reason_codes', '16', 'Lung', 10, 0, 0, '', '4', '', 0, 0, 1, '', 1),
+('clinikal_reason_codes', '17', 'Blood Vessel', 20, 0, 0, '', '4', '', 0, 0, 1, '', 1),
+('clinikal_reason_codes', '18', 'Backbone', 10, 0, 0, '', '5', '', 0, 0, 1, '', 1),
+('clinikal_reason_codes', '19', 'Brain', 20, 0, 0, '', '5', '', 0, 0, 1, '', 1),
+('clinikal_reason_codes', '20', 'Echocardiography', 10, 0, 0, '', '6', '', 0, 0, 1, '', 1),
+('clinikal_reason_codes', '21', 'Echo In Effort', 20, 0, 0, '', '6', '', 0, 0, 1, '', 1),
+('clinikal_reason_codes', '22', 'Holter Blood Pressure', 30, 0, 0, '', '6', '', 0, 0, 1, '', 1);
+#EndIf
+
+#IfNotRow fhir_value_sets id service_types
+INSERT INTO `fhir_value_sets` (`id`, `title`) VALUES ('service_types', 'Service Types');
+#EndIf
+
+#IfNotRow2D fhir_value_set_systems vs_id service_types system clinikal_service_types
+INSERT INTO `fhir_value_set_systems` (`vs_id`, `system`, `type`) VALUES ('service_types', 'clinikal_service_types', 'All');
+#EndIf
+
+#IfNotRow fhir_value_sets id reason_codes_1
+INSERT INTO `fhir_value_sets` (`id`, `title`) VALUES ('reason_codes_1', 'Ultrasound Reason Codes');
+#EndIf
+
+#IfNotRow2D fhir_value_set_systems vs_id reason_codes_1 system clinikal_reason_codes
+INSERT INTO `fhir_value_set_systems` (`vs_id`, `system`, `type`, `filter`) VALUES ('reason_codes_1', 'clinikal_reason_codes', 'Filter', '1');
+#EndIf
+
+#IfNotRow fhir_value_sets id reason_codes_2
+INSERT INTO `fhir_value_sets` (`id`, `title`) VALUES ('reason_codes_2', 'Mammography Reason Codes');
+#EndIf
+
+#IfNotRow2D fhir_value_set_systems vs_id reason_codes_2 system clinikal_reason_codes
+INSERT INTO `fhir_value_set_systems` (`vs_id`, `system`, `type`, `filter`) VALUES ('reason_codes_2', 'clinikal_reason_codes', 'Filter', '2');
+#EndIf
+
+#IfNotRow fhir_value_sets id reason_codes_3
+INSERT INTO `fhir_value_sets` (`id`, `title`) VALUES ('reason_codes_3', 'X-ray Reason Codes');
+#EndIf
+
+#IfNotRow2D fhir_value_set_systems vs_id reason_codes_3 system clinikal_reason_codes
+INSERT INTO `fhir_value_set_systems` (`vs_id`, `system`, `type`, `filter`) VALUES ('reason_codes_3', 'clinikal_reason_codes', 'Filter', '3');
+#EndIf
+
+#IfNotRow fhir_value_sets id reason_codes_4
+INSERT INTO `fhir_value_sets` (`id`, `title`) VALUES ('reason_codes_4', 'CT Reason Codes');
+#EndIf
+
+#IfNotRow2D fhir_value_set_systems vs_id reason_codes_4 system clinikal_reason_codes
+INSERT INTO `fhir_value_set_systems` (`vs_id`, `system`, `type`, `filter`) VALUES ('reason_codes_4', 'clinikal_reason_codes', 'Filter', '4');
+#EndIf
+
+#IfNotRow fhir_value_sets id reason_codes_5
+INSERT INTO `fhir_value_sets` (`id`, `title`) VALUES ('reason_codes_5', 'MRI Reason Codes');
+#EndIf
+
+#IfNotRow2D fhir_value_set_systems vs_id reason_codes_5 system clinikal_reason_codes
+INSERT INTO `fhir_value_set_systems` (`vs_id`, `system`, `type`, `filter`) VALUES ('reason_codes_5', 'clinikal_reason_codes', 'Filter', '5');
+#EndIf
+
+#IfNotRow fhir_value_sets id reason_codes_6
+INSERT INTO `fhir_value_sets` (`id`, `title`) VALUES ('reason_codes_6', 'Cardiology Reason Codes');
+#EndIf
+
+#IfNotRow2D fhir_value_set_systems vs_id reason_codes_6 system clinikal_reason_codes
+INSERT INTO `fhir_value_set_systems` (`vs_id`, `system`, `type`, `filter`) VALUES ('reason_codes_6', 'clinikal_reason_codes', 'Filter', '6');
+#EndIf
+
+#IfNotRow fhir_value_sets id reason_codes_7
+INSERT INTO `fhir_value_sets` (`id`, `title`) VALUES ('reason_codes_7', 'Biopsy Reason Codes');
+#EndIf
+
+#IfNotRow2D fhir_value_set_systems vs_id reason_codes_7 system clinikal_reason_codes
+INSERT INTO `fhir_value_set_systems` (`vs_id`, `system`, `type`, `filter`) VALUES ('reason_codes_7', 'clinikal_reason_codes', 'Filter', '7');
+#EndIf
+
+#IfNotRow2D list_options list_id lists option_id clinikal_enc_statuses
+INSERT INTO `list_options` (`list_id`, `option_id`, `title`, `seq`, `is_default`, `option_value`, `mapping`, `notes`, `codes`, `toggle_setting_1`, `toggle_setting_2`, `activity`, `subtype`, `edit_options`) VALUES
+('lists', 'clinikal_enc_statuses', 'Clinikal Encounter Statuses', 0, 0, 0, '', '', '', 0, 0, 1, '', 1),
+('clinikal_enc_statuses', '1', 'Planned', 10, 0, 0, '', '', '', 0, 0, 1, '', 1),
+('clinikal_enc_statuses', '2', 'Arrived', 20, 0, 0, '', '', '', 0, 0, 1, '', 1),
+('clinikal_enc_statuses', '3', 'Triaged', 30, 0, 0, '', '', '', 0, 0, 1, '', 1),
+('clinikal_enc_statuses', '4', 'In Progress', 40, 0, 0, '', '', '', 0, 0, 1, '', 1),
+('clinikal_enc_statuses', '5', 'Waiting For Results', 50, 0, 0, '', '', '', 0, 0, 1, '', 1),
+('clinikal_enc_statuses', '6', 'Finished', 60, 0, 0, '', '', '', 0, 0, 1, '', 1),
+('clinikal_enc_statuses', '7', 'Cancelled', 15, 0, 0, '', '', '', 0, 0, 1, '', 1);
+#EndIf
+
+#IfNotRow fhir_value_sets id encounter_statuses
+INSERT INTO `fhir_value_sets` (`id`, `title`) VALUES ('encounter_statuses', 'Encounter Statuses');
+#EndIf
+
+#IfNotRow2D fhir_value_set_systems vs_id encounter_statuses system clinikal_enc_statuses
+INSERT INTO `fhir_value_set_systems` (`vs_id`, `system`, `type`) VALUES ('encounter_statuses', 'clinikal_enc_statuses', 'All');
+#EndIf
+
+#IfNotRow2D list_options list_id lists option_id clinikal_app_statuses
+INSERT INTO `list_options` (`list_id`, `option_id`, `title`, `seq`, `is_default`, `option_value`, `mapping`, `notes`, `codes`, `toggle_setting_1`, `toggle_setting_2`, `activity`, `subtype`, `edit_options`) VALUES
+('lists', 'clinikal_app_statuses', 'Clinikal Appointment Statuses', 0, 0, 0, '', '', '', 0, 0, 1, '', 1),
+('clinikal_app_statuses', '1', 'Pending', 10, 0, 0, '', '', '', 0, 0, 1, '', 1),
+('clinikal_app_statuses', '2', 'Booked', 20, 0, 0, '', '', '', 0, 0, 1, '', 1),
+('clinikal_app_statuses', '3', 'Arrived', 30, 0, 0, '', '', '', 0, 0, 1, '', 1),
+('clinikal_app_statuses', '4', 'Cancelled', 40, 0, 0, '', '', '', 0, 0, 1, '', 1),
+('clinikal_app_statuses', '5', 'No Show', 50, 0, 0, '', '', '', 0, 0, 1, '', 1),
+('clinikal_app_statuses', '6', 'Waitlisted', 60, 0, 0, '', '', '', 0, 0, 1, '', 1);
+#EndIf
+
+#IfNotRow fhir_value_sets id patient_tracking_statuses
+INSERT INTO `fhir_value_sets` (`id`, `title`) VALUES ('patient_tracking_statuses', 'Appointment Statuses For Patients Tracking');
+#EndIf
+
+#IfNotRow2D fhir_value_set_systems vs_id patient_tracking_statuses system clinikal_app_statuses
+INSERT INTO `fhir_value_set_systems` (`vs_id`, `system`, `type`) VALUES ('patient_tracking_statuses', 'clinikal_app_statuses', 'Partial');
+INSERT INTO `fhir_value_set_codes` (`vss_id`, `code`) VALUES
+(LAST_INSERT_ID(), 1),
+(LAST_INSERT_ID(), 2),
+(LAST_INSERT_ID(), 4),
+(LAST_INSERT_ID(), 5);
 #EndIf
 
 -- no appropriate condition
@@ -172,10 +335,14 @@ UPDATE `list_options` SET `list_id` = 'clinikal_service_categories' WHERE list_i
 -- no appropriate condition
 UPDATE `list_options` SET `option_id` = 'clinikal_service_categories' WHERE option_id = 'fhir_service_categories';
 
+#IfNotRow fhir_rest_elements name ValueSet
+INSERT INTO `fhir_rest_elements` (`name`, `active`) VALUES
+('ValueSet', 1);
+#EndIf
 
 #IfNotRow fhir_rest_elements name Encounter
-INSERT INTO `fhir_rest_elements` (`id`, `name`, `active`) VALUES
-(5, 'Encounter', 1);
+INSERT INTO `fhir_rest_elements` (`name`, `active`) VALUES
+('Encounter', 1);
 #EndIf
 
 #IfMissingColumn form_encounter status
