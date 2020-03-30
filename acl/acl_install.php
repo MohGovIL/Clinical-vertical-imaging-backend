628ee37bf27694a38a8d0e78567f19a97abc60ab
eyalvo-matrix <eyalvo.matrix@gmail.com>
2020-03-30 14:49:57 +0300

    PC-381

--- acl/acl_install.php
+++ acl/acl_install.php
@@ -4,17 +4,18 @@
 //$receptionist_view = @addNewACL('Vaccine receptionists', 'receptionist', 'view', 'Things that receptionists can read but not modify');
 //$receptionist_write = @addNewACL('Vaccine receptionists', 'receptionist', 'write', 'Things that receptionists can modify');
 
-@addNewACL('Imaging technician', 'imaging_technician', 'write', 'Things that imaging technician can modify');
-@addNewACL('Imaging technician', 'imaging_technician', 'view', 'Things that imaging technician can read but not modify');
-@addNewACL('Imaging doctor', 'imaging_doctor', 'write', 'Things that imaging doctor can modify');
-@addNewACL('Imaging doctor', 'imaging_doctor', 'view', 'Things that imaging doctor can read but not modify');
-@addNewACL('Imaging manager', 'imaging_clinic_manager', 'write', 'Things that imaging clinic manager can modify');
-@addNewACL('Imaging manager', 'imaging_clinic_manager', 'view', 'Things that imaging clinic manager can read but not modify');
-@addNewACL('Imaging representative', 'imaging_call_center_representative', 'write', 'Things that imaging call center representative can modify');
-@addNewACL('Imaging representative', 'imaging_call_center_representative', 'view', 'Things that imaging call center representative can read but not modify');
-@addNewACL('Imaging receptionist', 'imaging_receptionist', 'write', 'Things that imaging receptionist can modify');
-@addNewACL('Imaging receptionist', 'imaging_receptionist', 'view', 'Things that imaging receptionist can read but not modify');
+$imaging_technician_write =@addNewACL('Imaging technician', 'imaging_technician', 'write', 'Things that imaging technician can modify');
+$imaging_technician_view =@addNewACL('Imaging technician', 'imaging_technician', 'view', 'Things that imaging technician can read but not modify');
+$imaging_doctor_write =@addNewACL('Imaging doctor', 'imaging_doctor', 'write', 'Things that imaging doctor can modify');
+$imaging_doctor_view =@addNewACL('Imaging doctor', 'imaging_doctor', 'view', 'Things that imaging doctor can read but not modify');
+$imaging_clinic_manager_write =@addNewACL('Imaging manager', 'imaging_clinic_manager', 'write', 'Things that imaging clinic manager can modify');
+$imaging_clinic_manager_view =@addNewACL('Imaging manager', 'imaging_clinic_manager', 'view', 'Things that imaging clinic manager can read but not modify');
+$imaging_call_center_representative_write =@addNewACL('Imaging representative', 'imaging_call_center_representative', 'write', 'Things that imaging call center representative can modify');
+$imaging_call_center_representative_view =@addNewACL('Imaging representative', 'imaging_call_center_representative', 'view', 'Things that imaging call center representative can read but not modify');
+$imaging_receptionist_write =@addNewACL('Imaging receptionist', 'imaging_receptionist', 'write', 'Things that imaging receptionist can modify');
+$imaging_receptionist_view =@addNewACL('Imaging receptionist', 'imaging_receptionist', 'view', 'Things that imaging receptionist can read but not modify');
 
+/**********************************************************/
 
 //using BY https://matrixil.sharepoint.com/:x:/r/sites/DTG/Clinical/_layouts/15/Doc.aspx?sourcedoc=%7B49114388-8709-407B-BF08-8E8F26C8119F%7D&file=%D7%A7%D7%9C%D7%99%D7%A0%D7%99%D7%A7%D7%9C%20%D7%93%D7%99%D7%9E%D7%95%D7%AA%20-%20%D7%94%D7%A8%D7%A9%D7%90%D7%95%D7%AA%20%D7%9C%D7%A4%D7%99%20%D7%AA%D7%A4%D7%A7%D7%99%D7%93%D7%99%D7%9D%20%D7%92%D7%A8%D7%A1%D7%94%201.xlsx&action=default&mobileredirect=true&cid=6559357a-ef09-499a-b410-0a6b0078e2e7
 //OBJECT OF ACL
