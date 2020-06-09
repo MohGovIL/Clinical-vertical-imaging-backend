8430f95dc4a4672705bbad29d4c513a3d47d8023
Amiel Elboim <amielel@matrix.co.il>
2020-06-09 15:18:58 +0300

    fix demo data

--- modules/Imaging/sql/demo_data.sql
+++ modules/Imaging/sql/demo_data.sql
@@ -15,8 +15,8 @@ VALUES
 
 REPLACE INTO `facility` (`id`, `name`, `phone`, `fax`, `street`, `city`, `state`, `postal_code`, `country_code`, `federal_ein`, `website`, `email`, `service_location`, `billing_location`, `accepts_assignment`, `pos_code`, `x12_sender_id`, `attn`, `domain_identifier`, `facility_npi`, `tax_id_type`, `color`, `primary_business_entity`, `facility_code`, `extra_validation`, `facility_taxonomy`, `mail_street`, `mail_street2`, `mail_city`, `mail_state`, `mail_zip`, `oid`, `iban`, `info`, `active`)
 VALUES
-('15', 'מרפאת תל אביב', NULL, NULL, NULL, NULL, NULL, NULL, '', NULL, NULL, NULL, '1', '0', '0', '11', NULL, NULL, NULL, NULL, '', '#95AFFF', '0', NULL, '1', NULL, NULL, NULL, NULL, NULL, NULL, '', NULL, NULL, '1'),
-('16', 'מרפאת חיפה', NULL, NULL, NULL, NULL, NULL, NULL, '', NULL, NULL, NULL, '1', '0', '0', '11', NULL, NULL, NULL, NULL, '', '#96AFFF', '0', NULL, '1', NULL, NULL, NULL, NULL, NULL, NULL, '', NULL, NULL, '1');
+('15', 'מרפאת תל אביב', NULL, NULL, NULL, NULL, NULL, NULL, '', NULL, NULL, NULL, '1', '1', '0', '11', NULL, NULL, NULL, NULL, '', '#95AFFF', '0', NULL, '1', NULL, NULL, NULL, NULL, NULL, NULL, '', NULL, NULL, '1'),
+('16', 'מרפאת חיפה', NULL, NULL, NULL, NULL, NULL, NULL, '', NULL, NULL, NULL, '1', '1', '0', '11', NULL, NULL, NULL, NULL, '', '#96AFFF', '0', NULL, '1', NULL, NULL, NULL, NULL, NULL, NULL, '', NULL, NULL, '1');
 
 INSERT INTO `facility` (`id`, `name`, `phone`, `fax`, `street`, `city`, `state`, `postal_code`, `country_code`, `federal_ein`, `website`, `email`, `service_location`, `billing_location`, `accepts_assignment`, `pos_code`, `x12_sender_id`, `attn`, `domain_identifier`, `facility_npi`, `tax_id_type`, `color`, `primary_business_entity`, `facility_code`, `extra_validation`, `facility_taxonomy`, `mail_street`, `mail_street2`, `mail_city`, `mail_state`, `mail_zip`, `oid`, `iban`, `info`, `active`) VALUES
 (17, 'מרפאת מטריקס', '099599989', '099591234', 'החרש', 'הוד השרון', '4', '5232568', 'ישראל', '', 'matrix.co.il', 'matrix@co.il', 0, 0, 0, 1, NULL, '', '', '3', 'EI', '#33CC99', 0, '1', 1, '', '', '', '', '', '', '', '', '', 1);
